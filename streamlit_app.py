6dccb06b156a60c3e8d783cc713c869a16c6ca1d
Balamurugan Muthu <Balamurugan.glakes@gmail.com>
2024-05-22 21:26:14 +0530

    Update streamlit_app.py
    DT

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,4 +32,4 @@ if ingredients_list:
 
 # display nutrition information
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), use_conainer_width=True)
