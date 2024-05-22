0df9d468c1e7605edb0a2bca53591be3d396a87d
Balamurugan Muthu <Balamurugan.glakes@gmail.com>
2024-05-22 21:56:25 +0530

    Update streamlit_app.py
    errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # Convert the Snowpark Dataframe to a Pandas Datraframe
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredients_list=st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5)
 if ingredients_list:
@@ -27,6 +27,12 @@ if ingredients_list:
 
     for each_fruit in ingredients_list:
         ingredients_string += each_fruit + ' '
+
+	search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
+        st.subheader(each_fruit + ' Nutrition Information')
+	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + each_fruit")
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
