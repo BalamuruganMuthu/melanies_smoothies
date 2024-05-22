eb8e4abb8bbd212652da9bee560faebec2e8cba1
Balamurugan Muthu <Balamurugan.glakes@gmail.com>
2024-05-22 21:59:49 +0530

    Update streamlit_app.py
    tabs

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,25 +23,23 @@ pd_df=my_dataframe.to_pandas()
 
 ingredients_list=st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5)
 if ingredients_list:
-    ingredients_string = ''
+	ingredients_string = ''
 
-    for each_fruit in ingredients_list:
-        ingredients_string += each_fruit + ' '
+    	for each_fruit in ingredients_list:
+		ingredients_string += each_fruit + ' '
+	        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+	        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+                st.subheader(each_fruit + ' Nutrition Information')
+	        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + each_fruit")
 
-	search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+                          values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
 
-        st.subheader(each_fruit + ' Nutrition Information')
-	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + each_fruit")
+        time_to_order = st.button('Submit Order')
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-            values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
-
-    time_to_order = st.button('Submit Order')
-
-    if time_to_order:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered!', icon="✅")
+        if time_to_order:
+	        session.sql(my_insert_stmt).collect()
+                st.success('Your Smoothie is ordered!', icon="✅")
 
 # # display nutrition information
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
