ad6fc45ea7881ce9517efedc423e6b5a155aefd1
Balamurugan Muthu <Balamurugan.glakes@gmail.com>
2024-05-22 22:04:28 +0530

    Update streamlit_app.py
    indent

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,22 +24,22 @@ pd_df=my_dataframe.to_pandas()
 ingredients_list=st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5)
 if ingredients_list:
 	ingredients_string = ''
-
-    	for each_fruit in ingredients_list:
+	
+	for each_fruit in ingredients_list:
 		ingredients_string += each_fruit + ' '
-	        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-	        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-                st.subheader(each_fruit + ' Nutrition Information')
-	        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + each_fruit")
-
-        my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-                          values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
-
-        time_to_order = st.button('Submit Order')
-
-        if time_to_order:
-	        session.sql(my_insert_stmt).collect()
-                st.success('Your Smoothie is ordered!', icon="✅")
+		search_on=pd_df.loc[pd_df['FRUIT_NAME'] == each_fruit, 'SEARCH_ON'].iloc[0]
+		st.write('The search value for ', each_fruit,' is ', search_on, '.')
+		st.subheader(each_fruit + ' Nutrition Information')
+		fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + each_fruit)
+	
+	my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order) 
+	values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
+	
+	time_to_order = st.button('Submit Order')
+	
+	if time_to_order:
+		session.sql(my_insert_stmt).collect()
+		st.success('Your Smoothie is ordered!', icon="✅")
 
 # # display nutrition information
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
